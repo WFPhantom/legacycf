c25a6c7f9f61c1633ebb009dec34d92eda3c2cd2
Veselin Plochev <wfphantom5@yahoo.com>
2023-06-11 23:59:40 +0300

    should be fine if user is not trying to break it

--- background.js
+++ background.js
@@ -1,18 +1,6 @@
-chrome.tabs.onActivated.addListener( function(activeInfo){
-  chrome.tabs.get(activeInfo.tabId, function(tab){
-      let url = tab.url;
-      if (url.startsWith("https://www.curseforge.com/")){
-       url = url.replace("www", "legacy")
-        chrome.tabs.update(tab.id, {url: url});
-      }
-  })
-});
-
-chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
-  if (tab.active && change.url) {
-    if (change.url.startsWith("https://www.curseforge.com/")){
+chrome.tabs.onUpdated.addListener((tabId, change) => {
+  if (change.url && change.url.startsWith("https://www.curseforge.com/")) {
       change.url = change.url.replace("www", "legacy")
-      chrome.tabs.update(tab.id, {url: change.url});
-  }          
+      chrome.tabs.update(tabId, {url: change.url});    
   }
-});
+})
