92689f488aa925310c340044ba534e016555b70e
Veselin Plochev <89593443+WFPhantom@users.noreply.github.com>
2023-08-06 21:14:06 +0300

    Update background.js for mozilla

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
-chrome.tabs.onUpdated.addListener((tabId, change) => {
+browser.tabs.onUpdated.addListener((tabId, change) => {
   if (change.url && change.url.startsWith("https://www.curseforge.com/")) {
       change.url = change.url.replace("www", "legacy")
-      chrome.tabs.update(tabId, {url: change.url});    
+      browser.tabs.update(tabId, {url: change.url});    
   }
 })
